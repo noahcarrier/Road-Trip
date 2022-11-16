dd07e6eec7f4d4b2287800ed883c5d5a53388516
Anna MacInnis <106559858+anna-sage@users.noreply.github.com>
2022-11-15 21:27:23 -0500

    Made dropdown on plantrip page clear after state added

--- src/scripts/plantrip.js
+++ src/scripts/plantrip.js
@@ -1,36 +1,18 @@
 // document elements
 const stateSelect = document.querySelector("#stateSelect");
+const addBtn = document.querySelector("#addBtn");
 // temporary add button i added just to test script - delete later
-const tempAdd = document.querySelector("#tempAdd");
-
-// Toggle all the state buttons to invisible
-for(let i = 0; i < 48; i++) {
-    //stateToggle(appropriate id);
-}
-
-
-// Adds a state to the trip
-function addState(stateId) {
-    console.log("adding state" + stateId + " to trip");
-}
 
 // Toggles a state button visible or invisible
-function stateToggle(state) {
-    if(state.style.display === "none") {
-        state.style.display = "block";
-    } else {
-        state.style.display = "none";
-    }
-}
 
 var trip = new Array();
 let counter = -1;
 
 // Updates trip array and unhides state
-tempAdd.addEventListener("click", function(e){
+addBtn.addEventListener("click", function(e){
     counter++;
     trip[counter] = Number(stateSelect.value);
-    addState(trip[counter]);
+    stateSelect.value = "blank";
 });
 
 done_btn.addEventListener("click", function() {
